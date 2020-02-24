41ab90fff3223fbec3f2cde7e219f8622dcad7e2
nimbusskye <alexwang6671@gmail.com>
2020-02-24 18:27:52 -0500

    Did lots of shit

--- Lab5b.asm
+++ Lab5b.asm
@@ -4,6 +4,150 @@
 .data
 .code
 
+; check if we have a special case of a horizontal or vertical line
+drawLine:
+color EQU ss:[bp+4]
+y2 EQU ss:[bp+6]
+x2 EQU ss:[bp+8]
+y1 EQU ss:[bp+10]
+x1 EQU ss:[bp+12]
+<<<<<<< HEAD
+
+push bp
+mov bp, sp
+
+deltaX equ ss:[bp - 2]
+deltaY equ ss:[bp - 4]
+slope equ ss:[bp - 6]
+y0 equ ss: [bp-8]
+y equ ss: [bp-10]
+x equ ss: [bp-12]
+sub sp, 12
+
+
+=======
+push bp
+mov bp, sp
+>>>>>>> 101d71e420358d64ba714998233330266210f892
+mov bx, x2
+mov ax, y2
+mov cx, x1
+mov dx, y1
+sub bx, cx	                ; BX = X2 -X1
+<<<<<<< HEAD
+mov deltaX, bx
+jz callh
+sub ax, dx                  ; AX = Y2 -Y1
+mov deltaY, ax
+jz callv
+
+xchg ax, bx
+
+div bx						; BX contains the slope
+mov slope, bx
+mov dx, bx					; Move slope to DX for loop
+
+mov ax, slope
+mov bx, x1
+mul bx
+mov ax, y1
+sub ax, bx					; b = mx1 - y1
+mov y0, ax					; y0 contains y-intercept
+
+mov bx, x1
+mov cx, y1
+mov x, bx
+mov y, cx
+
+outer:
+mov ax, x2
+mov bx, x
+cmp ax, bx
+jz exit
+inner:
+mov ax, y
+mov bx, y2
+cmp ax, bx
+jz finishith
+mov ax, slope
+mul bx						; Multiply x by slope
+mov dx, y0
+sub dx, bx					; DX contains y(x)
+cmp cx, dx					; Check if y = y(x)	
+jz callpix
+finishith:
+mov cx, y
+inc cx
+mov y, cx
+mov ax, y2
+cmp cx, ax
+jnz inner
+mov ax, x
+inc ax
+mov x, ax
+jmp outer
+
+callv:
+mov ax, y2
+mov bx, y1
+mov cx, x1
+mov dx, color
+push ax
+push bx
+push cx
+push dx
+call drawLine_v
+mov sp, bp
+pop bp
+ret 10
+
+callh:
+mov ax, x2
+mov bx, y1
+mov cx, x1
+mov dx, color
+push ax
+push bx
+push cx
+push dx
+call  drawLine_h
+mov sp, bp
+pop bp
+ret 10
+
+callpix:
+push cx
+push bx
+push color
+=======
+jz  drawLine_h
+sub ax, si                  ; AX = Y2 -Y1
+jz drawLine_v
+finit                           ; initialize FPU
+; make sure we are drawing along the right axis, so that our slope is <= 1
+cmp ax, bx 
+;ja short .dlf_yorient		; (Y2 -Y1) > (X2 -X1) ? If so, change axis of drawing
+fild WORD PTR [bp+12]           ; ST(0) = Y1(floating point value on FPU stack)
+fld st(0)                   ; ST(1) = ST(0) = Y1
+fisubr WORD PTR[bp+8]          ; ST(0) = Y2 -Y1
+fild WORD PTR[bp+10]         ; ST(0) = X2
+fisub WORD PTR[bp+14]         ; ST(0) = X2 -X1    
+fdiv
+; ST(0) = (X2 -X1)/(Y2 -Y1)(slope), ST(1) = Y1
+add cx, 1                   ; next X
+fadd st(0), st(1)            	; calculate next Y (prevY + slope)
+fist WORD PTR[bp-14]         ; overwrite stack argument Y
+mov [bp-12], cx
+pop bp             ; overwrite stack argument X
+>>>>>>> 101d71e420358d64ba714998233330266210f892
+call drawPixel
+jmp finishith
+
+exit:
+mov sp, bp
+pop bp
+ret 10
+
 ; draw a single pixel specific to Mode 13h (320x200 with 1 byte per color)
 drawPixel:
 	color EQU ss:[bp+4]
@@ -52,177 +196,73 @@ drawLine_h:
 	color EQU ss:[bp+4]
 	x1 EQU ss:[bp+6]
 	y1 EQU ss:[bp+8]
-	x2 EQU ss:[bp+10]
+	X2 EQU ss:[bp+10]
+
+	push bp
+	mov bp, sp
+
+	push    bx
+	push    cx
+
+	; BX keeps track of the X coordinate
+	mov	bx, x1
+
+	; CX = number of pixels to draw
+	mov	cx, x2
+	sub	cx, bx
+	inc	cx
+	dlh_loop:
+		push	y1
+		push	bx
+		push	color
+		call	drawPixel
+		add	bx, 1
+		loopw	dlh_loop
+	dlh_end:
+
+	pop     cx
+	pop     bx
+
+	pop bp
+
+	ret 8
+
+
+; draw a vertical line
+drawLine_v:
+	color EQU ss:[bp+4]
+	x1 EQU ss:[bp+6]
+	y1 EQU ss:[bp+8]
 	y2 EQU ss:[bp+12]
 
 	push bp
 	mov bp, sp
 
-	push ax
 	push    bx
 	push    cx
-	push dx
 
-	; BX keeps track of the X coordinate
-	mov	bx, x1
+	; BX keeps track of the Y coordinate
+	mov	bx, y1
 
-	; CX = number of horizontal pixels to draw
-	mov	cx, x2
+	; CX = number of pixels to draw
+	mov	cx, y2
 	sub	cx, bx
 	inc	cx
-
-	; AX keeps track of the Y coordinate
-	mov ax, y1
-
-	; DX = number of vertical pixels to draw
-	mov dx, y2
-	sub dx, ax
-	inc dx
-
-	dlh_loop:
-		push    ax
+	dlv_loop:
 		push	bx
+		push	x1
 		push	color
 		call	drawPixel
-		inc bx
-        inc ax
-		dec dx
-        dec cx
-        jnz dlh_loop
+		add	bx, 1
+		loopw	dlv_loop
+	dlv_end:
 
-	dlh_end:
-
-	pop dx
 	pop     cx
 	pop     bx
-	pop ax
 
 	pop bp
 
-	ret 10
-
-
-; draw a vertical line
-; drawLine_v:
-; 	color EQU ss:[bp+4]
-; 	x1 EQU ss:[bp+6]
-; 	y1 EQU ss:[bp+8]
-; 	y2 EQU ss:[bp+10]
-
-; 	push bp
-; 	mov bp, sp
-
-; 	push    bx
-; 	push    cx
-
-; 	; BX keeps track of the Y coordinate
-; 	mov	bx, y1
-
-; 	; CX = number of pixels to draw
-; 	mov	cx, y2
-; 	sub	cx, bx
-; 	inc	cx
-; 	dlv_loop:
-; 		push	bx
-; 		push	x1
-; 		push	color
-; 		call	drawPixel
-; 		add	bx, 1
-; 		loopw	dlv_loop
-; 	dlv_end:
-
-; 	pop     cx
-; 	pop     bx
-
-; 	pop bp
-
-; 	ret 8
-
-
-; ; draw a right increasing diagonal line
-; drawLine_d1:
-; 	color EQU ss:[bp+4]
-; 	x1 EQU ss:[bp+6]
-; 	y1 EQU ss:[bp+8]
-; 	x2 EQU ss:[bp+10]
-
-; 	push bp
-; 	mov bp, sp
-
-; 	push    bx
-; 	push    cx
-; 	push	dx
-
-; 	; BX keeps track of the X coordinate,
-; 	; DX keeps track of the Y coordinate
-; 	mov	bx, x1
-; 	mov	dx, y1
-
-; 	; CX = number of pixels to draw
-; 	mov	cx, x2
-; 	sub	cx, bx
-; 	inc	cx
-; 	dld1_loop:
-; 		push	dx
-; 		push	bx
-; 		push	color
-; 		call	drawPixel
-; 		add	bx, 1
-; 		sub	dx, 1
-; 		loopw	dld1_loop
-; 	dld1_end:
-
-; 	pop	dx
-; 	pop     cx
-; 	pop     bx
-;     pop ax
-
-; 	pop bp
-
-; 	ret 8
-
-
-; ; draw a right decreasing diagonal line
-; drawLine_d2:
-; 	color EQU ss:[bp+4]
-; 	x1 EQU ss:[bp+6]
-; 	y1 EQU ss:[bp+8]
-; 	x2 EQU ss:[bp+10]
-
-; 	push bp
-; 	mov bp, sp
-
-; 	push    bx
-; 	push    cx
-; 	push	dx
-
-; 	; BX keeps track of the X coordinate,
-; 	; DX keeps track of the Y coordinate
-; 	mov	bx, x1
-; 	mov	dx, y1
-
-; 	; CX = number of pixels to draw
-; 	mov	cx, x2
-; 	sub	cx, bx
-; 	inc	cx
-; 	dld2_loop:
-; 		push	dx
-; 		push	bx
-; 		push	color
-; 		call	drawPixel
-; 		add	bx, 1
-; 		add	dx, 1
-; 		loopw	dld2_loop
-; 	dld2_end:
-
-; 	pop	dx
-; 	pop     cx
-; 	pop     bx
-
-; 	pop bp
-
-; 	ret 8
-
+	ret 8
 
 start:
 	; initialize data segment
@@ -237,47 +277,50 @@ start:
 	; draw a house
 
 	; left wall
-	; push WORD PTR 190
-	; push WORD PTR 110
-	; push WORD PTR 60
-	; push 0001h
-	; call drawLine_v
+	push WORD PTR 190
+	push WORD PTR 110
+	push WORD PTR 60
+	push WORD PTR 60
+	push 0001h
+	call drawLine
 
-	; ; right wall
-	; push WORD PTR 190
-	; push WORD PTR 110
-	; push WORD PTR 260
-	; push 0002h
-	; call drawLine_v
+	; right wall
+	push WORD PTR 190
+	push WORD PTR 110
+	push WORD PTR 260
+	push 0002h
+	call drawLine
 
 	; top
 	push WORD PTR 260
 	push WORD PTR 110
-	push WORD PTR 300
-    push WORD PTR 300
+	push WORD PTR 60
 	push 0003h
-	call drawLine_h
+	call drawLine
 
 	; floor
-	; push WORD PTR 260
-	; push WORD PTR 190
-	; push WORD PTR 60
-	; push 0004h
-	; call drawLine_h
+	push WORD PTR 260
+	push WORD PTR 190
+	push WORD PTR 60
+	push WORD PTR 50
+	push 0004h
+	call drawLine
 			
-	; ; roof left
-	; push WORD PTR 160
-	; push WORD PTR 110
-	; push WORD PTR 60
-	; push 0005h
-	; call drawLine_d1
+	; roof left
+	push WORD PTR 160
+	push WORD PTR 110
+	push WORD PTR 60
+	push WORD PTR 50
+	push 0005h
+	call drawLine
 
-	; ; roof right
-	; push WORD PTR 260
-	; push WORD PTR 10
-	; push WORD PTR 160
-	; push 0006h
-	; call drawLine_d2
+	; roof right
+	push WORD PTR 260
+	push WORD PTR 10
+	push WORD PTR 160
+	push WORD PTR 20
+	push 0006h
+	call drawLine
 
 	; prompt for a key
 	mov ah, 0
